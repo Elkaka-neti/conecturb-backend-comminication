46af94ab9189e52729c63a0808e21f38136c055c
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-11 15:28:11 +0000

    [CREATE] Iniciando projeto com Index dinâmica que entende as rotas por arquivos separados

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const registerRoutes = require('./utils/registerRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+        res.send("hello");
+        });
+
+
+
+        // Registrar rotas automaticamente
+const routerDir = path.join(__dirname, 'routers');
+registerRoutes(app, routerDir);
+
+
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+module.exports = app;
