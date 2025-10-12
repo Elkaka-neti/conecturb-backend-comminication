7914ecc342d75a3018f0b5c50dfe8348a8e65dcf
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-12 16:34:48 -0300

    [UPDATE] Corrigindo identação de código

--- utils/registerRouters.js
+++ utils/registerRouters.js
@@ -5,43 +5,37 @@ function registerRoutes(app, routerPath) {
 
 if (!fs.existsSync(routerPath)) return;
 
-    const methodDirs = fs.readdirSync(routerPath, { withFileTypes: true })
-                    .filter(dirent => dirent.isDirectory())
-                                    .map(dirent => dirent.name);
+const methodDirs = fs.readdirSync(routerPath, { withFileTypes: true })
+.filter(dirent => dirent.isDirectory())
+.map(dirent => dirent.name);
 
-                                            methodDirs.forEach(method => {
-                                                            const methodPath = path.join(routerPath, method);
+methodDirs.forEach(method => {
+  const methodPath = path.join(routerPath, method);
+  const routeFiles = fs.readdirSync(methodPath, { withFileTypes: true })
+    .filter(dirent => dirent.isFile() && dirent.name.endsWith('.js'))
+    .map(dirent => dirent.name);
 
-                                                                            const routeFiles = fs.readdirSync(methodPath, { withFileTypes: true })
-                                                                                                    .filter(dirent => dirent.isFile() && dirent.name.endsWith('.js'))
-                                                                                                                            .map(dirent => dirent.name);
+  routeFiles.forEach(file => {
+    const routeName = '/' + path.parse(file).name;
+    const handler = require(path.join(methodPath, file));
+    switch (method.toLowerCase()) {
+      case 'get':
+        app.get(routeName, handler);
+      break;
+      case 'post':
+        app.post(routeName, handler);
+      break;
+      case 'put':
+        app.put(routeName, handler);
+      break;
+      case 'delete':
+        app.delete(routeName, handler);
+      break;
+      default:
+        console.warn(`Método HTTP não suportado: ${method}`);
+    }
+  });
+});
+}
 
-                                                                                                                                            routeFiles.forEach(file => {
-                                                                                                                                                                    const routeName = '/' + path.parse(file).name;
-                                                                                                                                                                                            const handler = require(path.join(methodPath, file));
-                                                                                                                                                                                                                    switch (method.toLowerCase()) {
-
-                                                                                                                                                                                                                                                    case 'get':
-                                                                                                                                                                                                                                                                                            app.get(routeName, handler);
-                                                                                                                                                                                                                                                                                                                                    break;
-
-                                                                                                                                                                                                                                                                                                                                                                    case 'post':
-                                                                                                                                                                                                                                                                                                                                                                                                            app.post(routeName, handler);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    break;
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    case 'put':
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            app.put(routeName, handler);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    break;
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    case 'delete':
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            app.delete(routeName, handler);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    break;
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    default:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            console.warn(`Método HTTP não suportado: ${method}`);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            }
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            module.exports = registerRoutes;
+module.exports = registerRoutes;
