d6340890071a4262f35bc2790d776b05429f6163
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-13 17:04:30 -0300

    [UPDATE] Refatoramento de código de alguns erros

--- src/Notifications/main.js
+++ src/Notifications/main.js
@@ -1,39 +1,38 @@
 const Whatsapp = require('./Whatsapp');
 const ExpoNot = require('./Mobile');
+
 class Notifications {
   static async create(user, messageTemplate, params = {}) {
+    if (!user) {
+      console.error("[404] Parâmetro 'user' não encontrado");
+      return;
+    }
+
     const channels = [];
-    if(!user) return console.error("[404] Parametro user nao encontrado");
-    
-    if(user.notifications.allows.includes("whatsapp")) {
-      channels.push(new Whatsapp());
-    }
-    if(user.notifications.allows.includes("expoApp")) {
-      channels.push(new ExpoNot());
-    }
-    
-    for(const ch of channels) {
+    if (user.notifications.allows.includes("whatsapp")) channels.push(new Whatsapp());
+    if (user.notifications.allows.includes("expoApp")) channels.push(new ExpoNot());
+
+    for (const ch of channels) {
       try {
-      await ch.send(user, messageTemplate, params);
-      }catch(e){
-        console.error("[500] Falha ao enviar mensagem.")
+        await ch.send(user, messageTemplate, params);
+      } catch (e) {
+        console.error(`[500] Falha ao enviar mensagem via :`, e.message);
       }
     }
   }
-  
+
   static async broadcast(users, messageTemplate, params = {}) {
     const lista = users.map(async user => {
       try {
-     await Notifications.create(user, messageTemplate, params)
-      }catch(e){
-        console.error("[500] Não foi possivel notificar esse usuario.")
+        await Notifications.create(user, messageTemplate, params);
+      } catch (e) {
+        console.error(`[500] Não foi possível notificar o usuário:`, e.message);
       }
     });
-    
-    //Mesmo se alguns falharem a promessa é resolvida.
+
+    // todas as promessas serao resolvidas, mesmo se algumas falharem
     return await Promise.allSettled(lista);
   }
-  
 }
 
 module.exports = Notifications;
