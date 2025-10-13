87f845f28c6dd051d2c1fac7883a2ceda6efa899
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-13 16:55:18 -0300

    [UPDATE] Removendo erro throw para evitar travamento de código

--- src/Notifications/main.js
+++ src/Notifications/main.js
@@ -3,7 +3,7 @@ const ExpoNot = require('./Mobile');
 class Notifications {
   static async create(user, messageTemplate, params = {}) {
     const channels = [];
-    if(!user) throw new ReferenceError("[404] Parametro user nao encontrado");
+    if(!user) return console.error("[404] Parametro user nao encontrado");
     
     if(user.notifications.allows.includes("whatsapp")) {
       channels.push(new Whatsapp());
@@ -18,8 +18,15 @@ class Notifications {
   }
   
   async broadcast(users, messageTemplate, params = {}) {
-    const lista = users.map(user => this.create(user, messageTemplate, params));
-    //Mesmo se alguns falharem a promessa e resolvida.
+    const lista = users.map(user => {
+      try {
+      this.create(user, messageTemplate, params)
+      }catch(e){
+        console.error("[500] Não foi possivel notificar esse usuario.")
+      }
+    });
+    
+    //Mesmo se alguns falharem a promessa é resolvida.
     return await Promise.allSettled(lista);
   }
   
