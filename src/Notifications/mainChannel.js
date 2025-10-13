7cfa64b23caa3de568067b48668c1c419e639a5f
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-13 16:56:45 -0300

    [UPDATE] Removendo erro throw para evitar travamento de código

--- src/Notifications/mainChannel.js
+++ src/Notifications/mainChannel.js
@@ -1,6 +1,6 @@
 class MainChannel {
   async send(user, messageTemplate, params) {
-    throw new InternalError("[502] O metodo só pode ser utilizado dentro de uma classe de agente.")
+    console.error("[502] O metodo só pode ser utilizado dentro de uma classe de agente.")
   }
 }
 
