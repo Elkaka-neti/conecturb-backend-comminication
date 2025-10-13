8588d0ccff91ded13a068fb236bd008b562f9ed9
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-13 16:16:41 -0300

    [UPDATE] Implementando modulo de verificação de envio de notificação para canais diferentes

--- src/Notifications/main.js
+++ src/Notifications/main.js
@@ -1,7 +1,22 @@
+const Whatsapp = require('./Whatsapp');
+const ExpoNot = require('./Mobile');
 class Notifications {
-  constructor() {
-    this.user,
-    this.message
+  static async create(user, messageTemplate, params = {}) {
+    const channels = [];
+    if(!user) throw new ReferenceError("[404] Parametro user nao encontrado");
+    
+    if(user.notifications.allows.includes("whatsapp")) {
+      channels.push(new Whatsapp());
+    }
+    if(user.notifications.allows.includes("expoApp")) {
+      channels.push(new ExpoNot());
+    }
+    
+    for(const ch of channels) {
+      ch.send(user, messageTemplate, params);
+    }
   }
   
 }
+
+module.exports = Notifications;
