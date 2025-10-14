f0c9bc9c2839e8127dee0cbbfc6067e697add200
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-14 12:36:56 -0300

    [CREATE] Criando módulo para melhor identificação de strings

---
+++ utils/eventTypes.js
@@ -0,0 +1,11 @@
+const eventTypes = {
+PEDIDO_CRIADO: "pedido.criado",
+LOJA_ACEITOU: "loja.aceitou",
+LOJA_RECUSOU: "loja.recusou",
+ENTREGADOR_ACEITOU: "entregador.aceitou",
+ENTREGADOR_RECUSOU: "entregador.recusou",
+PEDIDO_ACEITO: "pedido.aceito",
+CLIENTE_RECUSOU: "cliente.recusou"
+}
+
+module.exports = eventTypes;
