a3d7532a2c8716d784c6bb384ac3327c0a709e74
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-14 12:39:53 -0300

    [UPDATE] Atualizando semântica de algumas expressões

--- utils/eventTypes.js
+++ utils/eventTypes.js
@@ -4,7 +4,7 @@ LOJA_ACEITOU: "loja.aceitou",
 LOJA_RECUSOU: "loja.recusou",
 ENTREGADOR_ACEITOU: "entregador.aceitou",
 ENTREGADOR_RECUSOU: "entregador.recusou",
-PEDIDO_ACEITO: "pedido.aceito",
+PEDIDO_CONCLUIDO: "pedido.concluido",
 CLIENTE_RECUSOU: "cliente.recusou"
 }
 
