ca1cf25fa212f3385b09d5a4b845d424b0e358f7
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-15 12:47:29 -0300

    [UPDATE] Criando a base do switch/case

--- src/Workers/pedidoWorker.js
+++ src/Workers/pedidoWorker.js
@@ -1,7 +1,18 @@
 const PedidoFila = require('../messaging/PedidoFila');
+const eventTypes = require("../utils/eventTypes.js");
 
 async function worker(event) {
-    // switch/case
+    switch(event.type) {
+      case eventTypes.PEDIDO_CRIADO:
+        //fun() {
+        break;
+      case eventTypes.PEDIDO_NOTICAR_LOJA:
+        //fun() {
+        break;
+      default:
+        //fun() {
+        break;
+    }
     console.log(`Processando evento: ${event.type} para pedido ${event.data.pedidoId}`);
 }
 
