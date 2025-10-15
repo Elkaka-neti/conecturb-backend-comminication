bd0aa8ce5ad0c0cba11905e4146ea1a6a844d918
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-15 12:52:44 -0300

    [UPDATE] Atualizando padrão para o orquestrador ter maior prioridade sobre o processo

--- utils/eventTypes.js
+++ utils/eventTypes.js
@@ -1,7 +1,9 @@
 const eventTypes = {
 PEDIDO_CRIADO: "pedido.criado",
+NOTIFICAR_LOJA: "pedido.notificar.loja",
 LOJA_ACEITOU: "loja.aceitou",
 LOJA_RECUSOU: "loja.recusou",
+NOTIFICAR_ENTREGADORES: "pedido.notificar.entrega",
 ENTREGADOR_ACEITOU: "entregador.aceitou",
 ENTREGADOR_RECUSOU: "entregador.recusou",
 PEDIDO_CONCLUIDO: "pedido.concluido",
