6fe4f8a00b030b831d247bfdb692183523cc8a0e
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-15 10:28:35 -0300

    [DELETE] Removendo lógica de switch do routers e mandando para algum service worker

--- routers/post/pedido.js
+++ routers/post/pedido.js
@@ -1,33 +1,6 @@
-const eventTypes = require("../utils/eventTypes.js")
 
 module.export = (req, res) => {
   const {data, type} = req.body;
   const {user, store, itens = []} = data;
   
-  switch(type) {
-    case eventTypes.PEDIDO_CRIADO:
-      //fun() {
-      break; 
-    case eventTypes.LOJA_ACEITOU:
-      //fun() {
-      break;
-    case eventTypes.LOJA_RECUSOU:
-      //fun() {
-      break;
-    case eventTypes.ENTREGADOR_ACEITOU:
-      //fun() {
-      break;
-    case eventTypes.ENTREGADOR_RECUSOU:
-      //fun() {
-      break;
-    case eventTypes.CLIENTE_RECUSOU:
-      //fun() {
-      break;
-    case eventTypes.PEDIDO_CONCLUIDO:
-      //fun() {
-      break;
-    default:
-      //fun default() {
-      break;
-  }
 }
